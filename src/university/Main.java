c8224d94181a3942d9256f4c9b2014c21c7aaa24
bl-niketb <niket.bhaskar@bridgelabz.com>
2023-06-05 09:11:47 +0530

    added new code

--- src/university/Main.java
+++ src/university/Main.java
@@ -8,8 +8,23 @@ public class Main {
 
         //Declaration of array
         int numbersArray[];
-        numbersArray = new int[]{2,4,6,8};
-        int max = numbersArray[0];
+        numbersArray = new int[]{2,4,6,8,10,12,14,16,18,20};
+        for(int i : main.sum(numbersArray)){
+            System.out.println(i);
+        };
+    }
 
+    int[] sum(int[] arr){
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
+        int arraynum[] = new int[]{1,2,3,4};
+        return arraynum;
+    }
+    String sum(int a, int b){
+        int sum = a+b;
+        return "some string";
     }
 }
